2afc42baa3172db928ea80032f29e819b91f3a65
pedrooliver13 <pedro@qyon.com>
2021-06-17 23:09:29 -0300

    Removendo comentários

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa');
-const isProd = process.env.NODE_ENV === 'production'; // vendo se está em produção.
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public', // destino que vou enviar o pwa
-    disable: !isProd // desabilitar isso quando ele tá em produção.
+    dest: 'public',
+    disable: !isProd
   }
 });
