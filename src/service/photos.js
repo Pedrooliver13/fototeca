c54147df99d458230bde4dd6c5565d66316a6f91
pedrooliver13 <pedro@qyon.com>
2021-11-29 11:07:35 -0300

    Fix skeleton effect

--- src/service/photos.js
+++ src/service/photos.js
@@ -1,7 +1,7 @@
 import client from 'service/api';
 
-export const getPhotos = async (query = 'Nature', pearPage = 40) => {
+export const getPhotos = async (pearPage = 40, page = 1, query = 'Nature') => {
   return client.photos
-    .search({ query, per_page: pearPage, page: 3 })
+    .search({ query, per_page: pearPage, page: page })
     .then((photos) => photos);
 };
