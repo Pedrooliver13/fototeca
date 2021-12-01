4f6117303f53decc1b7709abd9c18d76f5b8e0a0
pedrooliver13 <pedro@qyon.com>
2021-12-01 20:25:34 -0300

    Fix default query

--- src/service/photos.js
+++ src/service/photos.js
@@ -1,6 +1,6 @@
 import client from 'service/api';
 
-export const getPhotos = (page = 1, perPage = 12, query = 'Little Dogs') => {
+export const getPhotos = (page = 1, perPage = 12, query = 'Nature') => {
   return client.photos
     .search({ query, per_page: perPage, page: page })
     .then((photos) => photos);
